fdf2f26bc9cef680b5954d75cc79f200f5ed807c
Jason Mohabir <jmohabir@stuy.edu>
2015-11-18 15:17:22 -0500

    Create Rational.java

---
+++ Rational.java
@@ -0,0 +1,42 @@
+public class Rational {
+   
+    private int numer;
+    private int denom;
+
+    // Default Setting
+    public Rational() {
+	numer = 0;
+	denom = 1;
+    }
+
+    public Rational(int n, int d) {
+	this();
+	// denominator cannot be zero
+	if (d == 0) {
+	    System.out.println("Invalid. Setting to 0/1");
+	}
+	else {
+	    numer=n;
+	    denom=d;
+	}
+    }
+
+    public String toString() {
+	return numer + "/" + denom;
+    }
+
+    // returns the most precise float version of the rational
+    public double floatValue() {
+	return ((double)numer)/denom;
+    }
+
+    public void multiply(Rational factor) {
+	numer *= factor.numer;
+	denom *= factor.denom;
+    }
+
+    public void divide(Rational factor) {
+	denom *= factor.numer;
+	numer *= factor.denom;
+    }
+    }
